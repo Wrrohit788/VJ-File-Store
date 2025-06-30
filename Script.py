539fc3e3b8ee25911f3a84810f6b6cba5e67c3e8
Wrrohit788 <154300370+Wrrohit788@users.noreply.github.com>
2025-06-30 18:12:44 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -10,12 +10,8 @@ class script(object):
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
-    
-    CAPTION = """<b>📂 ғɪʟᴇɴᴀᴍᴇ : {file_name}
 
-⚙️ sɪᴢᴇ : {file_size}
 
-Jᴏɪɴ [ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ](https://t.me/motulink)</b>""" 
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
