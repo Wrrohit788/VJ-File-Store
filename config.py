8e9a18c3bfbb92f33923096c484efa47eb3daa57
Wrrohit788 <154300370+Wrrohit788@users.noreply.github.com>
2025-07-28 17:39:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -80,7 +80,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://local-ardeen-fsdvzcvzxv-db9faf59.koyeb.app/")
+URL = environ.get("URL", "https://yelling-florentia-fsdvzcvzxv-dd17cef1.koyeb.app/")
 
 
 # Don't Remove Credit Tg - @VJ_Botz
